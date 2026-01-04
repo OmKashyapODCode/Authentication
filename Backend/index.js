1ee5d86d8c16ec4ec0cc95ab6a2112b312d3dc0b
OmKashyapODCode <kashyap.om8585@gmail.com>
2026-01-04 12:14:04 +0530

    cors add

--- Backend/index.js
+++ Backend/index.js
@@ -1,38 +1,40 @@
 import express from 'express';
 import dotenv from "dotenv";
 import connectDB from './config/db.js';
-import {createClient} from "redis";
+import { createClient } from "redis";
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 
 dotenv.config();
 await connectDB();
 
-// Initialize Redis connection
-const redisURL = process.env.REDIS_URL;
-
-// Check if Redis URL is provided in the environment variables
-if (!redisURL) {
-    console.log("missing redis url");
-    process.exit(1); // Stop the server if no Redis URL is found
-}
-
-// Create a Redis client instance using the provided URL
-export const redisClient = createClient({
-    url: redisURL,
-});
-
-// Connect to Redis and handle connection success or errors
-redisClient
-    .connect()
-    .then(() => console.log("connected to redis"))
-    .catch(console.error);
-
-
 const app = express();
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL, // Netlify URL
+    credentials: true
+}));
 
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
+
+// Initialize Redis connection
+const redisURL = process.env.REDIS_URL;
+
+if (!redisURL) {
+    console.log("missing redis url");
+    process.exit(1);
+}
+
+export const redisClient = createClient({
+    url: redisURL,
+});
+
+redisClient
+    .connect()
+    .then(() => console.log("connected to redis"))
+    .catch(console.error);
 
 //import routes 
 import userRoutes from './routes/user.js';
@@ -43,4 +45,4 @@ app.use('/api/v1', userRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
-})
+});
