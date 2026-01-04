7409f07c15b25d07545204b504919bbc2bb57b6c
OmKashyapODCode <kashyap.om8585@gmail.com>
2026-01-04 12:26:20 +0530

    fix: vercel serverless backend

--- Backend/index.js
+++ Backend/index.js
@@ -11,38 +11,25 @@ await connectDB();
 const app = express();
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL, // Netlify URL
-    credentials: true
+  origin: process.env.FRONTEND_URL,
+  credentials: true
 }));
 
-//middlewares
 app.use(express.json());
 app.use(cookieParser());
 
-// Initialize Redis connection
+// Redis
 const redisURL = process.env.REDIS_URL;
-
 if (!redisURL) {
-    console.log("missing redis url");
-    process.exit(1);
+  console.log("missing redis url");
+  process.exit(1);
 }
 
-export const redisClient = createClient({
-    url: redisURL,
-});
+export const redisClient = createClient({ url: redisURL });
+await redisClient.connect();
 
-redisClient
-    .connect()
-    .then(() => console.log("connected to redis"))
-    .catch(console.error);
-
-//import routes 
+// routes
 import userRoutes from './routes/user.js';
-
-//using routes
 app.use('/api/v1', userRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
-});
+export default app;
