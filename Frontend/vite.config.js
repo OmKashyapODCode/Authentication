032e11544e020eb10054de8afd7696a20ec588f6
OmKashyapODCode <kashyap.om8585@gmail.com>
2026-01-04 11:58:23 +0530

    add netlify config and fix vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import autoprefixer from 'autoprefixer';
 
-const BACKEND_URL = 'http://localhost:5000';
+const BACKEND_URL = 'http://localhost:5000'
 
 export default defineConfig({
-  plugins: [react(),tailwindcss(),autoprefixer()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   server: {
     proxy: {
       '/api/v1': {
@@ -16,4 +18,4 @@ export default defineConfig({
       },
     },
   },
-});
+})
