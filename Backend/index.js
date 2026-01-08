941190e2e334e9e6c95502ad6f4824089f23c4ad
OmKashyapODCode <kashyap.om8585@gmail.com>
2026-01-08 22:35:39 +0530

    backend deployemt requirement check

--- Backend/index.js
+++ Backend/index.js
@@ -10,10 +10,7 @@ await connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
